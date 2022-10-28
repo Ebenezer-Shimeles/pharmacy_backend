7c3f3ba90b7f7018713c76dd6d40516f38480873
Ebenezer <ebenezertesfaye@yahoo.com>
2022-10-28 06:27:51 +0000

    added forgotten password and other auth for retailers too

--- src/retailer/retailer.controller.ts
+++ src/retailer/retailer.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Post,Body, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post,Body, Get, UseGuards, Request, Logger, BadRequestException, Patch } from '@nestjs/common';
+import { ChangePasswordFromInsideInput, ChangePasswordFromOutsideInput, VerifyCompanyDto } from 'src/app.dto';
+import { IsCompanyVerified } from 'src/company.strategy';
 import { CompanyInputDTO, CompanyOutputDTO } from 'src/app.dto';
 import { IsRetailerAuthenticated } from 'src/company.strategy';
 import { RetailerService } from './retailer.service';
@@ -25,6 +27,57 @@ export class RetailerController {
         output.phoneNumber = provider.phoneNumber;
 
         return output;
+
+   }
+   @UseGuards(IsRetailerAuthenticated, IsCompanyVerified)
+   @Get('test')
+   async test(@Request() request){
+       return request.user;
+   }
+   
+   @UseGuards(IsRetailerAuthenticated, IsCompanyVerified,)
+   @Patch('password') //this is to change the password from inside the app
+   async changePasswordFromInsideTheApp(@Request() request, @Body() input: ChangePasswordFromInsideInput){
+        if(!await this.retailerService.changePassword(request.user, input))
+           throw new BadRequestException({ error: "Error old password does not match"})
+       else return {msg: "Password changed"}
+   }   
+   
+   @Post('password')
+   async forgottenPassword(@Body() input: ChangePasswordFromOutsideInput){
+       //This is used for outside the app
+       // if there is a veification code and a new Password it acts to change the password
+       // else it will send a verification code to the phone number
+       if(input.password && input.verificationCode && input.phoneNumber){
+           if( await this.retailerService.changePasswordUsingCode(input)){
+               return {msg: "Password changed"}
+           }
+           else{
+               throw new BadRequestException({error: "Bad verification code given!"
+               })
+           }
+       }
+       else{
+           const genResult =  await this.retailerService.generateVerificationCode(input.phoneNumber)
+           if(genResult)
+               return {msg: "Code Sent!"}
+           throw new BadRequestException({error: "Error verification code already "})
+       }
+       
+       
+       throw new BadRequestException({error: "Bad Request"})
+
+   }
+
+   @UseGuards(IsRetailerAuthenticated)
+   @Post('verification')
+   async verify(@Request() request, @Body() input: VerifyCompanyDto){
+        Logger.log({input}, 'Test')
+        if(!await this.retailerService.verify(request.user, input))
+           throw new BadRequestException({error: "Error wrong code given"})
+       return {
+           msg: "Verified!"
+       }
 
    }
 }
--- src/retailer/retailer.service.ts
+++ src/retailer/retailer.service.ts
@@ -1,12 +1,54 @@
-import { Injectable, BadRequestException,  } from '@nestjs/common';
+import { Injectable, BadRequestException,Logger  } from '@nestjs/common';
 import { Retailer } from './retailer.model';
 import * as bc from 'bcrypt';
+import { ChangePasswordFromInsideInput, ChangePasswordFromOutsideInput, VerifyCompanyDto } from 'src/app.dto';
+import {generateVerficationCode} from 'src/utils';
 import { CompanyInputDTO } from 'src/app.dto';
 
 @Injectable()
 export class RetailerService {
   async findByPhoneNumber(phoneNumber: string) : Promise<Retailer | null>{
     return await Retailer.findOneBy({phoneNumber})
+}
+async changePassword(user: Retailer, input: ChangePasswordFromInsideInput) : Promise<boolean>{
+  const userInfo = await this.findByPhoneNumber(user.phoneNumber)
+  if(!userInfo) return false;
+
+  if(!await bc.compare(input.oldPassword,  userInfo.password)) return false;
+  const salt = await bc.genSalt();
+  userInfo.password = await bc.hash(input.newPassword, salt);
+  userInfo.verificationCode = null;
+  await userInfo.save()
+  return true;
+}
+async generateVerificationCode(phoneNumber: string) : Promise<boolean>{
+  const user = await this.findByPhoneNumber(phoneNumber);
+  if(user.verificationCode) return false;
+  user.phoneNumber = phoneNumber;
+  user.verificationCode = generateVerficationCode() + "";
+  await user.save();
+  return true;
+}
+async changePasswordUsingCode(input: ChangePasswordFromOutsideInput): Promise<boolean>{
+      const user = await this.findByPhoneNumber(input.phoneNumber);
+      if(user.verificationCode != input.verificationCode) return false;
+      const salt = await bc.genSalt();
+      user.password = await bc.hash( input.password, salt);
+      user.verificationCode = null;
+      await user.save()
+      return true
+}
+async verify(pro: Retailer, input: VerifyCompanyDto): Promise<boolean>{
+
+  const retailer = await this.findByPhoneNumber(pro.phoneNumber);
+  if(!retailer || !retailer.verificationCode) return false;
+  
+  Logger.log(`${Number(retailer.verificationCode)} != ${input.verificationCode}`)
+  if((retailer.verificationCode) != input.verificationCode) return false;
+  retailer.isVerified = true;
+  retailer.verificationCode = null;
+  await retailer.save();
+  return true;
 }
     async create(input: CompanyInputDTO) : Promise<Retailer>{
         if(await Retailer.findOneBy({phoneNumber: input.phoneNumber}) ){
@@ -21,6 +63,7 @@ export class RetailerService {
         retailer.tinNumber = input.tinNumber;
         retailer.password =  await bc.hash(input.password, salt);
         retailer.phoneNumber = input.phoneNumber;
+        retailer.verificationCode = generateVerficationCode() + "";
         await retailer.save()
         return retailer
   }
